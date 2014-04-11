32d866cfe0264490654687162005b0c560b5c01a
artesgo <artesgo@gmail.com>
2014-04-11 14:09:38 -0400

    pickups and scoremanager

---
+++ Assets/Pickup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+	void OnTriggerEnter(Collider col){
+		if (col.tag == "Player"){
+			Debug.Log("Coins!");
+			ScoreManager.score += 1;
+			Debug.Log(ScoreManager.score);
+		}
+	}
+	
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+	public static int score;
+	// Use this for initialization
+	void Start () {
+		score = 0;
+	}
+}
